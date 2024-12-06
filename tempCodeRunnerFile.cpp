7720aef54a9b92217c3e3df14eaa1b96d71aae47
DanitaKornkumpon <danita.kornkumpon@gmail.com>
2024-12-06 13:19:07 +0700

    lab7_2.cpp complete

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name,movie,day;
+    int no,gear;
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+    cout << "?????: ";
+    getline(cin, name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+    cout << name << ": ";
+    getline(cin, no);
+    gear = no/10000000 - 12;
+    cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you." << endl;
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+    cout << name << ": ";
+    getline(cin, movie);
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+    cout << name << ": ";
+    getline(cin, day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
+    cout << name << ": " << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/" << endl;
+    return 0;
+}
